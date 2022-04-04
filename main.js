95e1745b5f3afec82f1f7890038aa9d2a9f65995
Veysel Demirel <hello@veysel.co>
2022-04-04 03:35:29 +0300

    Clean junk codes.

--- main.js
+++ main.js
@@ -1,75 +1,81 @@
 import { alphabet } from "./constants/turkish-alphabet.js";
 
-const keys = document.querySelectorAll(".key");
-const keyArray = Array.from(keys);
+const keyArray = [...document.querySelectorAll(".key")];
 
 /**
- * Ripple Click Effect For Keys
+ * @description Create audio elements with data-key attribute.
  */
-keyArray.forEach((key) => {
-  key.onclick = function (event) {
-    let ripple = document.createElement("span");
-    ripple.classList.add("ripple");
-    this.appendChild(ripple);
-
-    let x = event.clientX - event.target.offsetLeft;
-    let y = event.clientY - event.target.offsetTop;
-
-    ripple.style.left = `${x}px`;
-    ripple.style.top = `${y}px`;
-
-    setTimeout(() => {
-      ripple.remove();
-    }, 300);
-  };
-});
-
-/**
- * Create "data-key" Attributes & Audio Elements
- */
-alphabet.forEach((item, index) => {
-  keyArray[index].setAttribute("data-key", alphabet[index].key);
-  let temporaryAudio = `
-  <audio data-key="${item.key}" src="./assets/sounds/${item.character}.mp3"></audio>
-    `;
-  document.getElementById("audios").innerHTML += temporaryAudio;
-});
-
-/**
- * Play Audio With "data-key" Attributes
- */
-function playAudioForKeyDown(event) {
-  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-  if (!audio) return;
-  audio.currentTime = 0;
-  audio.play();
-  key.classList.add("active");
-  setTimeout(() => {
-    key.classList.remove("active");
-  }, 200);
+function createAudioElements() {
+  alphabet.forEach((letter, index) => {
+    keyArray[index].setAttribute("data-key", alphabet[index].key);
+    const tempAudioElement = `
+    <audio data-key="${letter.key}" src="./assets/sounds/${letter.character}.mp3"></audio>
+      `;
+    const audiosElement = document.getElementById("audios");
+    audiosElement.innerHTML += tempAudioElement;
+  });
 }
 
-function playAudioForClick() {
-  let dataKey = this.getAttribute("data-key");
-  alphabet.forEach((item) => {
-    if (dataKey == item.key) {
-      const audio = document.querySelector(`audio[data-key="${item.key}"]`);
-      if (!audio) return;
-      audio.currentTime = 0;
-      audio.play();
+/**
+ * @description Play audio for keydown event.
+ * @param {Event} event
+ */
+function playAudioForKeyDown(event) {
+  const audioElement = document.querySelector(
+    `audio[data-key="${event.keyCode}"]`
+  );
+  const keyElement = document.querySelector(
+    `.key[data-key="${event.keyCode}"]`
+  );
+  audioElement.currentTime = 0;
+  audioElement.play();
+  keyElement.classList.add("active");
+  setTimeout(() => {
+    keyElement.classList.remove("active");
+  }, 300);
+}
+
+/**
+ * @description Play audio for key click.
+ */
+function playAudioForClick(event) {
+  const dataKey = event.srcElement.getAttribute("data-key");
+  alphabet.forEach((letter) => {
+    if (dataKey == letter.key) {
+      const audioElement = document.querySelector(
+        `audio[data-key="${letter.key}"]`
+      );
+      audioElement.currentTime = 0;
+      audioElement.play();
     }
   });
 }
 
 /**
- * Listen Click For Keys
+ * @description Handle click for key.
  */
-keys.forEach((key) => {
-  key.addEventListener("click", playAudioForClick);
-});
+function clickForKey() {
+  keyArray.forEach((key) => {
+    key.onclick = function (event) {
+      playAudioForClick(event);
 
-/**
- * Listen Keydown For Window Object
- */
+      let ripple = document.createElement("span");
+      ripple.classList.add("ripple");
+      this.appendChild(ripple);
+
+      let x = event.clientX - event.target.offsetLeft;
+      let y = event.clientY - event.target.offsetTop;
+
+      ripple.style.left = `${x}px`;
+      ripple.style.top = `${y}px`;
+
+      setTimeout(() => {
+        ripple.remove();
+      }, 300);
+    };
+  });
+}
+
+createAudioElements();
+clickForKey();
 window.addEventListener("keydown", playAudioForKeyDown);
